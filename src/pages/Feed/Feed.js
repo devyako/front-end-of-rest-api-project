01e7e23420175462582bb0c303025267b4fa1fb6
devyako <yakhyoismoildjonov@gmail.com>
2020-05-03 04:26:17 +0500

    update

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -50,7 +50,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch("http://localhost:8080/feed/posts?page=" + page)
+    fetch("http://localhost:8080/feed/posts?page=" + page, {
+      headers: {
+        Authorization: this.props.token,
+      },
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error("Failed to fetch posts.");
@@ -120,9 +124,13 @@ class Feed extends Component {
       url = "http://localhost:8080/feed/post/" + this.state.editPost._id;
       method = "PUT";
     }
+
     fetch(url, {
       method: method,
       body: formData,
+      headers: {
+        Authorization: this.props.token,
+      },
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
@@ -176,6 +184,9 @@ class Feed extends Component {
     this.setState({ postsLoading: true });
     fetch("http://localhost:8080/feed/post/" + postId, {
       method: "DELETE",
+      headers: {
+        Authorization: this.props.token,
+      },
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
